a83774850a78df6c7e8fc0d2aa5c9596f997268d
Jemal Hargett <jemal.hargett@icloud.com>
2020-06-18 20:12:38 -0400

    Updates

---
+++ precache-manifest.40486fd2bee85d4d5804db16b5965abc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6806faad78a6ae2b80e3bdae2d9c76",
+    "url": "/Meal-Chef/index.html"
+  },
+  {
+    "revision": "32e880af2481a034ded6",
+    "url": "/Meal-Chef/static/css/main.23948687.chunk.css"
+  },
+  {
+    "revision": "4258b76b78986d4df2da",
+    "url": "/Meal-Chef/static/js/2.491249ca.chunk.js"
+  },
+  {
+    "revision": "32e880af2481a034ded6",
+    "url": "/Meal-Chef/static/js/main.f41c9728.chunk.js"
+  },
+  {
+    "revision": "51df1af102af2c5079c8",
+    "url": "/Meal-Chef/static/js/runtime-main.3d65c6bb.js"
+  },
+  {
+    "revision": "f33b1c88a2303e79d9233d6922d90151",
+    "url": "/Meal-Chef/static/media/Search.f33b1c88.svg"
+  },
+  {
+    "revision": "c156626153c3d0711b6880ab4bbf13a8",
+    "url": "/Meal-Chef/static/media/foodBackground.c1566261.jpg"
+  },
+  {
+    "revision": "c0386850b36ff826a53ce429d4cfad43",
+    "url": "/Meal-Chef/static/media/macarons.c0386850.jpg"
+  },
+  {
+    "revision": "0c29a0abb4c468280d52437fe1f134bf",
+    "url": "/Meal-Chef/static/media/muffin.0c29a0ab.jpg"
+  },
+  {
+    "revision": "ffd2bf5e114a0a50f62731e4ea233b59",
+    "url": "/Meal-Chef/static/media/spaghetti.ffd2bf5e.jpg"
+  },
+  {
+    "revision": "7fd468614264e649b1d35e13abb1be79",
+    "url": "/Meal-Chef/static/media/taco.7fd46861.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Meal-Chef/precache-manifest.40486fd2bee85d4d5804db16b5965abc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Meal-Chef/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
